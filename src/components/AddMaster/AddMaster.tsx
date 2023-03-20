cb1639a194abd230009c380e5844a052d3cb5037
kansiree <kansiree_print@hotmail.com>
2023-03-20 22:03:58 +0700

    add page add master

--- src/components/AddMaster/AddMaster.tsx
+++ src/components/AddMaster/AddMaster.tsx
@@ -1,14 +1,56 @@
-import { Container } from "react-bootstrap";
-
+import React from "react";
+import { Button, Container, FloatingLabel, Form } from "react-bootstrap";
+import ConfigApplication from '../../application.json'
 export default function AddMaster() {
 
-   // const URL = (ConfigApplication.developMode=="Y")?ConfigApplication.API_URL_LOCAL:ConfigApplication.API_URL;
+    const URL = (ConfigApplication.developMode==="Y")?ConfigApplication.API_URL_LOCAL:ConfigApplication.API_URL;
     console.log("URL: "+URL);
    
+    const type = React.useRef<HTMLSelectElement>(null);
+    const detail = React.useRef<HTMLSelectElement>(null);
+    const btSave = React.useRef<HTMLButtonElement>(null);
+
+    const onClickSave = () =>{
+        let valueType = type.current?.value
+        let valueDetail = detail.current?.value
+        const data = { type: valueType,
+        fullName: valueDetail}
+        if(data.fullName==''){
+            alert('please key detail.');
+        }else if(data.type=='-1'){
+            alert('please select type.');
+    
+        }else{
+    
+            fetch(URL+'/createMaster',{
+                method: 'POST',
+                mode:'no-cors',
+                body: JSON.stringify(data)
+            }).then(res => alert('Save Success'))
+        }
+    }
+
     return(
+        <Form>
             <Container>
-                Add Master 1
+                <FloatingLabel controlId="floatingSelect" label="Select Type" >
+                    <Form.Select aria-aria-label="Floating label select example" ref={type}>
+                        <option value="-1">Select Type</option>
+                        <option value="system">System</option>
+                        <option value="aircraft">Aircraft</option>
+                        <option value="technical">Technical</option>
+                    </Form.Select>
+                </FloatingLabel>
+                <FloatingLabel controlId="floatingText" label="Detail" style={{marginTop:10}}>
+                    <Form.Control as="textarea" >
+                        
+                    </Form.Control>
+                </FloatingLabel>
+                <Button variant="primary" type="submit" ref={btSave} style={{marginTop:10}}
+                onClick={onClickSave}
+                >SAVE</Button>
             </Container>
+        </Form>
     )
     
 }
